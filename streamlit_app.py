6289a6912b88f4958997f10ec0928d0fcc6ec541
gamesbykk <127334503+gamesbykk@users.noreply.github.com>
2024-06-08 08:22:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import requests
 
-st.title('🎈 App Name')
+API_URL = "https://api-inference.huggingface.co/models/microsoft/rho-math-1b-v0.1"
+headers = {"Authorization": "Bearer hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
 
-st.write('Hello world!')
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
